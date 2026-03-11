1e81eda953de25b83292abe4b417061adc3e0c58
Yash Mehta <yashmehta1009@gmail.com>
2026-03-11 09:49:24 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,26 @@
 
 import express from 'express';
-import { ENV } from './lib/env.js';
+
+import path from 'path';
+import { ENV } from './lib/env.js'; 
 const app = express();
 
-app.get('/',(req, res) => {
+//gives the current directory path
+const __dirname = path.resolve();
+
+
+app.get('/yash',(req, res) => {
     res.send('Hello World!');
 });
+
+//make our app ready for development and production
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend','dist','index.html'));
+    });
+}
 
 app.listen(ENV.PORT ,() => {
     console.log(`Server is running on port ${ENV.PORT}`);
