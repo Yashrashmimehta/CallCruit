0175226649a9c8d772155f7dc61fe6fd746bbd33
Yash Mehta <yashmehta1009@gmail.com>
2026-03-14 11:55:54 +0530

    Implement session management with create, join, and end session functionalities; add session model and protect routes middleware

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -9,6 +9,7 @@ if (!apiKey || !apiSecret) {
   console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
 }
 
+//why did u not write new here? because StreamChat.getInstance already returns a singleton instance, so we don't need to create a new instance using the 'new' keyword. It ensures that we have only one instance of the StreamChat client throughout our application, which is more efficient and helps manage resources better.
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
 export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 
